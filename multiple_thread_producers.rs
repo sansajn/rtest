fa3ae59354798828567fe6a813d6e57add289d24
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-04-21 08:03:03 +0200

    multiple thread producers

--- multiple_thread_producers.rs
+++ multiple_thread_producers.rs
@@ -1,4 +1,4 @@
-// thread channel comunication sample
+// multiple thread producers by cloning transmitter sample 
 
 use std::thread;
 use std::sync::mpsc;  // Multiple Producer Single Consumer
@@ -7,12 +7,27 @@ use std::time::Duration;
 fn main() {
 	let (tx, rx) = mpsc::channel();
 
+	let tx1 = mpsc::Sender::clone(&tx);
 	thread::spawn(move || {
 		let vals = vec![
 			String::from("hi"),
 			String::from("from"),
 			String::from("the"),
 			String::from("thread")
+		];
+
+		for val in vals {
+			tx1.send(val).unwrap();
+			thread::sleep(Duration::from_millis(250));
+		}
+	});
+
+	thread::spawn(move || {
+		let vals = vec![
+			String::from("more"),
+			String::from("messages"),
+			String::from("for"),
+			String::from("you")
 		];
 
 		for val in vals {
