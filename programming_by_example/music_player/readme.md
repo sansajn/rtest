5dc541e17c8b6782792a4648358e767acd932533
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2020-09-01 17:31:46 +0200

    early cover image support

---
+++ programming_by_example/music_player/readme.md
@@ -0,0 +1,3 @@
+dependencies: libgtk-3-dev
+
+> `sudo apt install libgtk-3-dev`
---
+++ programming_by_example/music_player/src/main.rs
@@ -0,0 +1,77 @@
+use std::env;
+use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{
+	Application, 
+	ApplicationWindow, 
+	WidgetExt, 
+	GtkWindowExt,
+	ContainerExt,
+	Adjustment,
+	Image,
+	ImageExt,
+	Scale,
+	ScaleExt
+};
+use gtk::Orientation::{Horizontal, Vertical};
+
+mod toolbar;
+
+use crate::toolbar::MusicToolbar;
+
+struct App {
+	adjustment: Adjustment,
+	cover: Image,
+	toolbar: MusicToolbar,
+	window: ApplicationWindow
+}
+
+impl App {
+	fn new(application: Application) -> Self {
+		let window = ApplicationWindow::new(&application);
+		window.set_title("Rusic");
+
+		let vbox = gtk::Box::new(Vertical, 0);
+		window.add(&vbox);
+
+		let toolbar = MusicToolbar::new();
+		vbox.add(toolbar.toolbar());
+
+		let cover = Image::new();
+		cover.set_from_file("cover.jpg");
+		vbox.add(&cover);
+
+		let adjustment = Adjustment::new(0.0, 0.0, 10.0, 0.0, 0.0, 0.0);
+		let scale = Scale::new(Horizontal, &adjustment);
+		scale.set_draw_value(false);
+		vbox.add(&scale);
+
+		window.show_all();
+
+		let app = App {
+			adjustment,
+			cover,
+			toolbar, 
+			window
+		};
+
+		app.connect_events();
+		app.connect_toolbar_events();
+		app
+	}
+
+	fn connect_events(&self) {}
+}
+
+
+fn main() {
+	let application = Application::new("com.rust-by-example", ApplicationFlags::empty())
+		.expect("Application initialization failed");
+
+	application.connect_startup(|application| {
+		let _app = App::new(application.clone());		
+	});
+
+	application.connect_activate(|_| {});
+
+	application.run(&env::args().collect::<Vec<_>>());
+}
